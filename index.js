6c319fd7b39c3d9942736c11e95a6c3df3ee8cc3
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-10-23 18:17:41 -0400

    Array find

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find(game => game.result === "W");
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
